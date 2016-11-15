004b4964ac5f2f2a9c9a9d2a75de7b60adbf96cd
shuya-abe <s-abe@ist.osaka-u.ac.jp>
2016-11-15 15:57:06 +0900

    graphviz.rb: ホストの出力を変更 triangle.conf: ホストのmacを設定

--- lib/view/graphviz.rb
+++ lib/view/graphviz.rb
@@ -15,13 +15,13 @@ module View
         end
         topology.links.each do |each|
           next unless nodes[each.dpid_a] && nodes[each.dpid_b]
-          gviz.add_edges nodes[each.dpid_a], nodes[each.dpid_b]
+          gviz.add_edges nodes[each.dpid_a], nodes[each.dpid_b], "arrowhead" => "none"
         end
         hosts = topology.hosts.each_with_object([]) do |each|
-          gviz.add_nodes(each.to_s, shape: 'box')
+          gviz.add_nodes(each.to_s, shape: 'ellipse')
         end
         topology.hslinks.each do |each|
-          gviz.add_edges each.mac_address.to_s, each.dpid.to_hex
+          gviz.add_edges each.mac_address.to_s, each.dpid.to_hex, "arrowhead" => "none"
         end
         gviz.output png: @output
       end
